5b834164bbfe4009bbd8e047ed6d8cb6f220a338
AcoderM <242905891+AcoderM-tech@users.noreply.github.com>
2026-08-20 16:50:14 +0500

    Enhance build script to set up Site and SocialApp
    Added Python script to configure Site and SocialApp for Google authentication.

--- build.sh
+++ build.sh
@@ -3,4 +3,27 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
-python manage.py createsuperuser --noinput || true
+python -c "
+import os
+from django.contrib.sites.models import Site
+from allauth.socialaccount.models import SocialApp
+
+site, _ = Site.objects.get_or_create(id=1, defaults={
+    'domain': 'buildai.acoderm.uz',
+    'name': 'BuildAI'
+})
+if site.domain != 'buildai.acoderm.uz':
+    site.domain = 'buildai.acoderm.uz'
+    site.name = 'BuildAI'
+    site.save()
+
+if not SocialApp.objects.filter(provider='google').exists():
+    app = SocialApp.objects.create(
+        provider='google',
+        name='Google',
+        client_id=os.environ.get('GOOGLE_CLIENT_ID', ''),
+        secret=os.environ.get('GOOGLE_CLIENT_SECRET', ''),
+    )
+    app.sites.add(site)
+print('Site va SocialApp tayyor')
+"
