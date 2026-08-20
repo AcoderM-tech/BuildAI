826b1f38023a8e0b09b4f4432e19aef663f3e079
AcoderM <242905891+AcoderM-tech@users.noreply.github.com>
2026-08-20 17:01:26 +0500

    Refactor site creation and update logic in build.sh

--- build.sh
+++ build.sh
@@ -8,14 +8,18 @@ import os
 from django.contrib.sites.models import Site
 from allauth.socialaccount.models import SocialApp
 
-site, _ = Site.objects.get_or_create(id=1, defaults={'domain': 'buildai.acoderm.uz', 'name': 'BuildAI'})
-if site.domain != 'buildai.acoderm.uz':
+site = Site.objects.filter(domain='buildai.acoderm.uz').first()
+if not site:
+    site = Site.objects.first()
+if site:
     site.domain = 'buildai.acoderm.uz'
     site.name = 'BuildAI'
     site.save()
+else:
+    site = Site.objects.create(domain='buildai.acoderm.uz', name='BuildAI')
 
 if not SocialApp.objects.filter(provider='google').exists():
     app = SocialApp.objects.create(provider='google', name='Google', client_id=os.environ.get('GOOGLE_CLIENT_ID',''), secret=os.environ.get('GOOGLE_CLIENT_SECRET',''))
     app.sites.add(site)
-print('Done')
+print('Done:', site.domain)
 "
